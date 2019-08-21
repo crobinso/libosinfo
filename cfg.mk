870f2bbca5eb59a703496000250476c580afbd96
Fabiano Fidêncio <fidencio@redhat.com>
2019-08-21 08:40:21 +0200

    cfg.mk: Update exclude_file_name_regexp--sc_bindtextdomain
    Let's *not* run the check only for tests files.
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- cfg.mk
+++ cfg.mk
@@ -101,8 +101,6 @@ sc_bracket_spacing_check:
 # We don't use this feature of maint.mk.
 prev_version_file = /dev/null
 
-exclude_file_name_regexp--sc_bindtextdomain = ^(libvirt-gconfig/tests|examples)/
-
 exclude_file_name_regexp--sc_trailing_blank = ^tests/isodata/.*|docs/object-model.fig
 
-exclude_file_name_regexp--sc_bindtextdomain = ^tools/.*.c|tests/.*.c
+exclude_file_name_regexp--sc_bindtextdomain = ^tests/.*.c
